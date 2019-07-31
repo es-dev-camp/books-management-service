02f89621c32e78aa4b564c44f6e300acbfdbf28c
kengop <kengop@gmail.com>
2019-07-31 09:48:47 +0900

    refs #20 Use lazyLoad for loading book cover

--- src/components/BooksList.vue
+++ src/components/BooksList.vue
@@ -1,43 +1,43 @@
 <template>
-  <v-container fill-height fluid grid-list-xl>
-    <v-layout row wrap justify-center>
-      <v-flex xs12>
-        <v-card>
-          <v-container>
-            <v-layout>
-              <v-toolbar>
-                <v-text-field prepend-icon='search'
-                              placeholder='Search'
-                              v-model='searchWords' />
-                <v-spacer></v-spacer>
-                <v-btn icon @click='UpdateList'>
-                  <v-icon>refresh</v-icon>
-                </v-btn>
-              </v-toolbar>
-            </v-layout>
-          </v-container>
+  <v-container fluid grid-list-xl>
+    <v-layout align-start justify-start column >
+      <v-container>
+        <v-layout>
+          <v-toolbar>
+            <v-text-field prepend-icon='search'
+                          placeholder='Search'
+                          v-model='searchWords' />
+            <v-spacer></v-spacer>
+            <v-btn icon @click='UpdateList'>
+              <v-icon>refresh</v-icon>
+            </v-btn>
+          </v-toolbar>
+        </v-layout>
+      </v-container>
 
-          <v-container fluid grid-list-sm>
-            <v-layout row wrap>
-              <v-flex xs4 sm3 md2 lg2 xl1 v-for='book in FilterdBooks' v-bind:key='book.ISBN'>
-                <v-tooltip bottom>
-                  <template v-slot:activator='{ on }'>
-                    <v-img :src='book.Cover' dark v-on='on' @click='ShowDetail(book)' width='100%' height='100%'/>
-                  </template>
+      <v-container grid-list-xs fluid>
+        <v-layout align-start justify-start row wrap >
+          <v-flex d-flex xs4 sm3 md2 lg2 xl1 v-for='book in FilterdBooks' v-bind:key='book.ISBN'>
+            <div>
+              <v-layout align-center justify-start column>
+                <v-flex d-flex>
+                  <img v-lazy='book.Cover' class="grey lighten-4" width="128px" height="166px" @click='ShowDetail(book)'/>
+                </v-flex>
+                <v-flex d-flex>
                   <span>{{ book.Title }}</span>
-                </v-tooltip>
-              </v-flex>
-            </v-layout>
-          </v-container>
+                </v-flex>
+              </v-layout>
+            </div>
+          </v-flex>
+        </v-layout>
+      </v-container>
 
-          <v-dialog :value='isShowDetail' @input='isShowDetail = false'
-            width='80%' max-width='700px'>
-            <BookDetail :Book='selectedBook' @close-dialog='isShowDetail = false'></BookDetail>
-          </v-dialog>
+      <v-dialog :value='isShowDetail' @input='isShowDetail = false'
+        width='80%' max-width='700px'>
+        <BookDetail :Book='selectedBook' @close-dialog='isShowDetail = false'></BookDetail>
+      </v-dialog>
 
-          <v-footer class='mt-5'></v-footer>
-        </v-card>
-      </v-flex>
+      <v-footer class='mt-5'></v-footer>
     </v-layout>
   </v-container>
 </template>
