6ef2240e16852724aa5f782190a21911ae42e9a5
Makoto Uwaya <makoto.uwaya@gmail.com>
2019-08-05 21:12:17 +0900

    refs #18 Using SignInModule

---
+++ src/views/SignIn.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <v-container>
+          <v-layout text-xs-center wrap>
+            <v-flex mb-4>
+              <v-btn color='primary' @click='signIn'>Google sign in</v-btn>
+            </v-flex>
+          </v-layout>
+    </v-container>
+  </v-app>
+</template>
+
+<script lang='ts'>
+import { Component, Vue } from 'vue-property-decorator';
+import { SignInModule } from '@/modules/SignInModule';
+
+@Component({
+  methods: SignInModule.mapActions(["signIn"])
+})
+export default class Login extends Vue {}
+</script>
+
+<style>
+
+</style>
