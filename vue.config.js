a3824a8ded8ecb3d2700c4ae1913612644574860
Makoto Uwaya <makoto.uwaya@gmail.com>
2019-08-05 21:49:18 +0900

    refs #19 Add eslint and editorconfig

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,16 @@
+const buildTarget = process.env.VUE_APP_BUILDTARGET;
+const devTool = process.env.VUE_APP_DEVTOOL;
+
+const entrypoint = 'src/main.ts';
+const outputDir = 'dist';
+
 module.exports = {
-  devServer: {
-    host: '0.0.0.0',
-    disableHostCheck: true
-  }
-}
+  productionSourceMap: false,
+  configureWebpack: {
+    devtool: !devTool ? false : devTool
+  },
+  pages: {
+    index: entrypoint
+  },
+  outputDir: outputDir
+};
