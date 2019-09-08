c38c0ae3f8a8b4405ae7ec1217ce4094a557d8c9
Makoto Uwaya <makoto.uwaya@gmail.com>
2019-09-09 00:29:30 +0900

    refs #42 Output git hash to file and refer from functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,24 @@
 import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 
+import { readFileSync } from 'fs';
+
+function readCommitHash() {
+  try {
+    return readFileSync('.gitinfo', { encoding: 'utf-8' });
+  } catch {
+    return '';
+  }
+}
+
 admin.initializeApp();
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
 export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!\nBooksManagement!");
+  console.log(`git hash: ${readCommitHash()}`);
+  response.send('Hello from Firebase! BooksManagement!');
 });
 
 export const onWriteBook = require('./onWriteBook').func;
