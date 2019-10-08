df560c87bcc66ea2d06b94716dff1c20828096f6
Makoto Uwaya <makoto.uwaya@gmail.com>
2019-10-09 01:16:15 +0900

    refs #49 Add slack notification

--- functions/src/onCreateBook.ts
+++ functions/src/onCreateBook.ts
@@ -1,9 +1,50 @@
 import * as functions from 'firebase-functions';
+import { App } from '@slack/bolt';
+// import IBook from '../../common/IBook';
+
+const botToken = functions.config().slack.bot_token;
+const app = new App({
+  token: botToken,
+  signingSecret: functions.config().slack.signing_secret
+});
+app.error(console.log);
 
 export const func = functions.firestore
   .document('book/{isbn}')
   .onCreate(async (snap, context) => {
     console.log('triggered onCreate Book (' + context.params.isbn + ')');
-    console.log(snap.data());
+    await sendMessage(snap.data());
     return 0;
   });
+
+// TODO: Common に移動されたら IBook で受け取るようにする
+async function sendMessage(book: any) {
+  try {
+    const result = await app.client.chat.postMessage({
+      token: botToken,
+      channel: functions.config().slack.notify_channel_id,
+      text: '',
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `:new: *${book.Title}*
+:book::book::book: 作者: ${book.Authors.join(' ')}
+出版社: ${book.Publisher} 出版日: ${book.PublishDate} ${book.Location ? '配置場所: ' + book.Location : '' }
+${book.Comment}`
+          },
+          accessory: {
+            type: 'image',
+            image_url: book.Cover,
+            alt_text: `${book.Title} 's image.`
+          }
+        }
+      ]
+    });
+    console.log(result);
+  }
+  catch (error) {
+    console.error(error);
+  }
+}
