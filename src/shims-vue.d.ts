057ec679e535fb0eb217c1b4dbef7ce17f85b6f3
Makoto Uwaya <makoto.uwaya@gmail.com>
2020-05-04 15:04:05 +0900

    refs #104 Using Vuetify loader

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -3,5 +3,13 @@ declare module '*.vue' {
   export default Vue;
 }
 
+declare module 'vuetify/lib' {
+  import Vue from 'vue';
+  export interface VFormObj extends Vue {
+    validate(): boolean;
+  }
+}
+
+declare module 'vuetify/lib/directives';
 declare module 'vuetify/lib/locale/ja';
 declare module 'vuetify/lib/util/colors';
