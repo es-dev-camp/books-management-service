5a449833bb0bacc0339ffa0418e05bfa48a5150b
Makoto Uwaya <makoto.uwaya@gmail.com>
2020-10-21 19:33:04 +0900

    refs #109 🗑 Remove unused config for babel

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,4 @@
 const plugins = ['require-context-hook'];
-if (process.env.NODE_ENV === 'production') {
-  plugins.push('transform-remove-console');
-}
-
 module.exports = (api) => ({
   presets: [['@vue/app', { useBuiltIns: 'entry' }]],
   ...(api.env('test') && { plugins }),
